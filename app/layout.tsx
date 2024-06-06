dceae23c05099a0169c3c4703bab5462f5998499
Koyannote <koyannote2180@gmail.com>
2024-06-07 00:11:53 +0700

    update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SHRE - Desing",
+  title: "SHRE - Design",
   description: "shre products",
 };
 
